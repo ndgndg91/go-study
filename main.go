c81d2a737152b5e3ab657247d28eb45fb3f5aade
ndgndg91 <ndgndg91@gmail.com>
2020-10-24 17:53:16 +0900

    <feat> hit url

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"log"
+	"net/http"
 
 	"github.com/ndgndg91/go-study/banking"
 	"github.com/ndgndg91/go-study/facade"
@@ -10,7 +12,31 @@ import (
 )
 
 func main() {
-	dictionary()
+	urls := []string{
+		"https://www.airbnb.com/",
+		"https://www.google.com/",
+		"https://www.amazon.com/",
+		"https://www.reddit.com/",
+		"https://soundcloud.com/",
+		"https://www.instagram.com/",
+		"https://dong-gil.com/",
+	}
+
+	for _, url := range urls {
+		hitURL(url)
+	}
+}
+
+var errRequestFail = errors.New("Request failed")
+
+func hitURL(url string) error {
+	fmt.Println("Checking: ", url)
+	resp, err := http.Get(url)
+	if err == nil || resp.StatusCode >= 400 {
+		return errRequestFail
+	}
+
+	return nil
 }
 
 func syntaxPractice() {
